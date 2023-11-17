45c5c3db7a0803916e4a296616331bd05ab31262
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-17 15:56:30 -0500

    Style new bar

--- src/BarNew.jsx
+++ src/BarNew.jsx
@@ -11,25 +11,27 @@ export function BarNew(props) {
 
   return (
     <div>
-      <h1>Add a Bar</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          Name: <input type="text" name="name" />
-        </div>
-        <div>
-          Location: <input type="text" name="location" />
-        </div>
-        <div>
-          Hours: <input type="text" name="hours" />
-        </div>
-        <div>
-          Specials: <input type="text" name="specials" />
-        </div>
-        <div>
-          Image URL: <input type="text" name="image_url" />
-        </div>
-        <button type="submit">Add Bar</button>
-      </form>
+      <div className="card text-bg-light" style={{margin: "25px"}}>
+        <h1 style={{marginTop: "10px"}}>Add a Bar</h1>
+        <form className="container" onSubmit={handleSubmit}>
+          <div className="row mb-3" style={{padding: "20px"}}>
+            Name: <input className="form-label" type="text" name="name" />
+          </div>
+          <div className="row mb-3" style={{padding: "20px"}}>
+            Location: <input className="form-label" type="text" name="location" />
+          </div>
+          <div className="row mb-3" style={{padding: "20px"}}>
+            Hours: <input className="form-label" type="text" name="hours" />
+          </div>
+          <div className="row mb-3" style={{padding: "20px"}}>
+            Specials: <input className="form-label" type="text" name="specials" />
+          </div>
+          <div className="row mb-3" style={{padding: "20px"}}>
+            Image URL: <input className="form-label" type="text" name="image_url" />
+          </div>
+          <button className="btn btn-primary" type="submit" style={{margin: "10px"}}>Add Bar</button>
+        </form>
+      </div>
     </div>
   );
 }
