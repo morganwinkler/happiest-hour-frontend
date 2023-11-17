25e81bded26b7b3f1da73f1d4da993d372606f09
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-17 10:40:30 -0500

    Style navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,16 @@
+
+
 export function Header() {
   return (
     <header>
-      <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand">Happiest Hour</a>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
       </nav>
     </header>
   );
