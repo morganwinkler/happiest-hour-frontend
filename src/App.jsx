22fbf3eac5ed5370f971c366ed91c25c944ac31e
Morgan Winkler <winkler.morgan1553@gmail.com>
2023-11-16 21:29:55 -0500

    Add base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { Content } from './Content';
+import { Footer } from './Footer';
+import { Header } from './Header';
+
+
+
 
 function App() {
 
   return (
-    <>
-     Hello World
-    </>
+   <div>
+    <Header />
+    <Content />
+    <Footer />
+   </div>
   )
 }
 
---
+++ src/Content.jsx
@@ -0,0 +1,7 @@
+export function Content() {
+  return (
+    <div>
+      <h1>Welcome to Happiest Hour!</h1>
+    </div>
+  );
+}
