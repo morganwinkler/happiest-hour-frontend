239952450dbf24a58fd5b9d38b4d863b3df0d117
Morgan Winkler <103147883+morganwinkler@users.noreply.github.com>
2023-12-07 14:17:49 -0500

    Update index to map through specials

--- src/BarsIndex.jsx
+++ src/BarsIndex.jsx
@@ -3,19 +3,31 @@
 export function BarsIndex(props) {
   return (
     <div className="card bg-light">
-      <div className="row" style={{margin: "25px"}}>
+      <div className="row" style={{ margin: "25px" }}>
         <h1>Happiest Hour Featured Bars</h1>
       </div>
-        <div className="row justify-content-center">
-          {props.bars.map((bar) => (
-            <div key={bar.id} className="card " style={{width: "18rem", margin: "5px"}}>
-              <h2 className="card-title" style={{margin: "10px"}}>{bar.name}</h2>
-              <img className="card-img-top" src={bar.image_url} style={{maxHeight: "250px"}} />
-              <p className="card-text" style={{margin: "5px"}}>{bar.specials}</p>
-              <button className="btn btn-primary" onClick={() => props.onShowBar(bar)}>More Info</button>
+      <div className="row justify-content-center">
+        {props.bars.map((bar) => (
+          <div key={bar.id} className="card " style={{ width: "18rem", margin: "5px" }}>
+            <h2 className="card-title" style={{ margin: "10px" }}>
+              {bar.name}
+            </h2>
+            <img className="card-img-top" src={bar.image_url} style={{ maxHeight: "250px" }} />
+
+            <div className="card-text" style={{ margin: "5px" }}>
+              {bar.specials.map((special) => (
+                <div key={special.id}>
+                  <p>{special.special}</p>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+
+            <button className="btn btn-primary" onClick={() => props.onShowBar(bar)}>
+              More Info
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
