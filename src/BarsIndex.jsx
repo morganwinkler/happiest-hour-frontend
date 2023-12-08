05f8710caf179e1cf893a7704f73b2038bfa3458
Morgan Winkler <103147883+morganwinkler@users.noreply.github.com>
2023-12-07 21:18:58 -0500

    Add filter by category component

--- src/BarsIndex.jsx
+++ src/BarsIndex.jsx
@@ -1,32 +1,46 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 
 export function BarsIndex(props) {
+  const [filter, setFilter] = useState("Liquor" || "Craft" || "Beer" || "Wine" || "Frozen" || "Shots" || "none");
+
   return (
     <div className="card bg-light">
       <div className="row" style={{ margin: "25px" }}>
         <h1>Happiest Hour Featured Bars</h1>
       </div>
+      <form className="row" style={{ margin: "10px" }}>
+        <h3>Filter by Category:</h3>
+        <input type="button" value="Liquor" onClick={(event) => setFilter(event.target.value)} />
+        <input type="button" value="Beer" onClick={(event) => setFilter(event.target.value)} />
+        <input type="button" value="Wine" onClick={(event) => setFilter(event.target.value)} />
+        <input type="button" value="Frozen" onClick={(event) => setFilter(event.target.value)} />
+        <input type="button" value="Shots" onClick={(event) => setFilter(event.target.value)} />
+        <input type="button" value="Craft" onClick={(event) => setFilter(event.target.value)} />
+      </form>
       <div className="row justify-content-center">
-        {props.bars.map((bar) => (
-          <div key={bar.id} className="card " style={{ width: "18rem", margin: "5px" }}>
-            <h2 className="card-title" style={{ margin: "10px" }}>
-              {bar.name}
-            </h2>
-            <img className="card-img-top" src={bar.image_url} style={{ maxHeight: "250px" }} />
+        {props.bars
+          .filter((bar) => bar.categories.includes(filter))
+          .map((bar) => (
+            <div key={bar.id} className="card " style={{ width: "18rem", margin: "5px" }}>
+              <h2 className="card-title" style={{ margin: "10px" }}>
+                {bar.name}
+              </h2>
+              <img className="card-img-top" src={bar.image_url} style={{ maxHeight: "250px" }} />
 
-            <div className="card-text" style={{ margin: "5px" }}>
-              {bar.specials.map((special) => (
-                <div key={special.id}>
-                  <p>{special.special}</p>
-                </div>
-              ))}
+              <div className="card-text" style={{ margin: "5px" }}>
+                {bar.specials.map((special) => (
+                  <div key={special.id}>
+                    <p>{special.special}</p>
+                  </div>
+                ))}
+              </div>
+
+              <button className="btn btn-primary" onClick={() => props.onShowBar(bar)}>
+                More Info
+              </button>
             </div>
-
-            <button className="btn btn-primary" onClick={() => props.onShowBar(bar)}>
-              More Info
-            </button>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
--- src/Content.jsx
+++ src/Content.jsx
@@ -5,9 +5,7 @@ import { BarNew } from "./BarNew";
 import { Modal } from "./Modal";
 import { BarsShow } from "./BarsShow";
 
-
 export function Content() {
-
   const [bars, setBars] = useState([]);
   const [isBarsVisible, setIsBarsVisible] = useState(false);
   const [currentBar, setCurrentBar] = useState({});
@@ -17,22 +15,22 @@ export function Content() {
     axios.get("http://localhost:3000/bars.json").then((response) => {
       console.log(response.data);
       setBars(response.data);
-    })
-  }
+    });
+  };
 
   const handleCreateBar = (params) => {
     console.log("handleCreateBar", params);
-    axios.post("http://localhost:3000/bars.json", params)
-  }
+    axios.post("http://localhost:3000/bars.json", params);
+  };
 
   const handleShowBar = (bar) => {
     setIsBarsVisible(true);
     setCurrentBar(bar);
-  }
+  };
 
   const handleClose = () => {
     setIsBarsVisible(false);
-  }
+  };
 
   const handleUpdateBar = (id, params) => {
     axios.patch(`http://localhost:3000/bars/${id}.json`, params).then((response) => {
@@ -50,21 +48,20 @@ export function Content() {
   };
 
   const handleDestroyBar = (bar) => {
-         console.log("handleDestroyBar", bar);
-         axios.delete(`http://localhost:3000/bars/${bar.id}.json`).then((response) => {
-           setBars(bars.filter((p) => p.id !== bar.id));
-           console.log(response);
-           handleClose();
-         });
-       };
+    console.log("handleDestroyBar", bar);
+    axios.delete(`http://localhost:3000/bars/${bar.id}.json`).then((response) => {
+      setBars(bars.filter((p) => p.id !== bar.id));
+      console.log(response);
+      handleClose();
+    });
+  };
 
-  
   useEffect(handleIndexBars, []);
 
   return (
     <div className="container text-center">
       <BarNew onCreateBar={handleCreateBar} />
-      <BarsIndex  bars={bars} onShowBar={handleShowBar} />
+      <BarsIndex bars={bars} onShowBar={handleShowBar} />
       <Modal show={isBarsVisible} onClose={handleClose}>
         <BarsShow bar={currentBar} onUpdateBar={handleUpdateBar} onDestroyBar={handleDestroyBar} />
       </Modal>
